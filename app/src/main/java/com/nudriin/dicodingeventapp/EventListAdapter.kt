e72202c8e5fb5aeb059bc6142d8845105eddd689
Nurdin <mrsunnysummer@gmail.com>
2024-10-13 19:31:24 +0700

    feat: create upcoming fragment functionality

---
+++ app/src/main/java/com/nudriin/dicodingeventapp/EventListAdapter.kt
@@ -0,0 +1,51 @@
+package com.nudriin.dicodingeventapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.nudriin.dicodingeventapp.data.response.ListEventsItem
+import com.nudriin.dicodingeventapp.databinding.EventCardBinding
+
+class EventListAdapter: ListAdapter<ListEventsItem, EventListAdapter.ViewHolder>(DIFF_CALLBACK) {
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<ListEventsItem>() {
+            override fun areItemsTheSame(
+                oldItem: ListEventsItem,
+                newItem: ListEventsItem
+            ): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(
+                oldItem: ListEventsItem,
+                newItem: ListEventsItem
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+    class ViewHolder(val binding: EventCardBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(event: ListEventsItem) {
+            Glide.with(binding.root.context)
+                .load(event.mediaCover)
+                .into(binding.eventImg)
+
+            binding.tvTitle.text = event.name
+            binding.tvAuthor.text = event.ownerName
+            binding.tvSummary.text = event.summary
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventListAdapter.ViewHolder {
+        val binding = EventCardBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: EventListAdapter.ViewHolder, position: Int) {
+        val event = getItem(position)
+        holder.bind(event)
+    }
+}
--- app/src/main/java/com/nudriin/dicodingeventapp/ui/upcoming/UpcomingFragment.kt
+++ app/src/main/java/com/nudriin/dicodingeventapp/ui/upcoming/UpcomingFragment.kt
@@ -7,32 +7,57 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.nudriin.dicodingeventapp.EventListAdapter
+import com.nudriin.dicodingeventapp.data.response.ListEventsItem
 import com.nudriin.dicodingeventapp.databinding.FragmentUpcomingBinding
 
 class UpcomingFragment : Fragment() {
 
     private var _binding: FragmentUpcomingBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var viewModel: UpcomingViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val upcomingViewModel =
-            ViewModelProvider(this).get(UpcomingViewModel::class.java)
-
         _binding = FragmentUpcomingBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textUpcoming
-        upcomingViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val layoutManager = LinearLayoutManager(context)
+        binding.rvUpcoming.layoutManager = layoutManager
+
+        viewModel = ViewModelProvider(this)[UpcomingViewModel::class.java]
+
+        viewModel.isLoading.observe(viewLifecycleOwner){
+            showLoading(it)
+        }
+
+        viewModel.eventList.observe(viewLifecycleOwner){eventList ->
+            setEventList(eventList)
+        }
+
+    }
+
+    private fun setEventList(eventList: List<ListEventsItem>){
+        val adapter = EventListAdapter()
+        adapter.submitList(eventList)
+        binding.rvUpcoming.adapter = adapter
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.GONE
+        }
     }
 
     override fun onDestroyView() {
