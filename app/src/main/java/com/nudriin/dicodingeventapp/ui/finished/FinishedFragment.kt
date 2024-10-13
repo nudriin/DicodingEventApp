dd152ea8c336cfd077b51a433ad3b32b5ba63578
Nurdin <mrsunnysummer@gmail.com>
2024-10-13 19:39:42 +0700

    feat: create finished fragment functionality

--- app/src/main/java/com/nudriin/dicodingeventapp/ui/finished/FinishedFragment.kt
+++ app/src/main/java/com/nudriin/dicodingeventapp/ui/finished/FinishedFragment.kt
@@ -7,32 +7,59 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.nudriin.dicodingeventapp.EventListAdapter
+import com.nudriin.dicodingeventapp.data.response.ListEventsItem
 import com.nudriin.dicodingeventapp.databinding.FragmentFinishedBinding
+import com.nudriin.dicodingeventapp.databinding.FragmentUpcomingBinding
+import com.nudriin.dicodingeventapp.ui.upcoming.UpcomingViewModel
 
 class FinishedFragment : Fragment() {
 
     private var _binding: FragmentFinishedBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var viewModel: FinishedViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val finishedViewModel =
-            ViewModelProvider(this).get(FinishedViewModel::class.java)
-
         _binding = FragmentFinishedBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textFinished
-        finishedViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val layoutManager = LinearLayoutManager(context)
+        binding.rvFinished.layoutManager = layoutManager
+
+        viewModel = ViewModelProvider(this)[FinishedViewModel::class.java]
+
+        viewModel.isLoading.observe(viewLifecycleOwner){
+            showLoading(it)
+        }
+
+        viewModel.eventList.observe(viewLifecycleOwner){eventList ->
+            setEventList(eventList)
+        }
+
+    }
+
+    private fun setEventList(eventList: List<ListEventsItem>){
+        val adapter = EventListAdapter()
+        adapter.submitList(eventList)
+        binding.rvFinished.adapter = adapter
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.GONE
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/nudriin/dicodingeventapp/ui/finished/FinishedViewModel.kt
+++ app/src/main/java/com/nudriin/dicodingeventapp/ui/finished/FinishedViewModel.kt
@@ -1,13 +1,51 @@
 package com.nudriin.dicodingeventapp.ui.finished
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.nudriin.dicodingeventapp.data.response.EventResponse
+import com.nudriin.dicodingeventapp.data.response.ListEventsItem
+import com.nudriin.dicodingeventapp.data.retrofit.ApiConfig
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class FinishedViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is finish Fragment"
+    private val _eventList = MutableLiveData<List<ListEventsItem>>()
+    val eventList: LiveData<List<ListEventsItem>> = _eventList
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    companion object {
+        private const val TAG = "UpcomingViewModel"
     }
-    val text: LiveData<String> = _text
+
+    init {
+        getFinishedEvent()
+    }
+
+    private fun getFinishedEvent(){
+        _isLoading.value = true
+
+        val client = ApiConfig.getApiService().getAllEvents("0")
+        client.enqueue(object: Callback<EventResponse> {
+            override fun onResponse(call: Call<EventResponse>, response: Response<EventResponse>) {
+                _isLoading.value = false
+                if(response.isSuccessful && response.body() != null){
+                    val body = response.body()
+
+                    _eventList.value = body?.listEvents
+                }
+            }
+
+            override fun onFailure(call: Call<EventResponse>, t: Throwable) {
+                _isLoading.value = false
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+
+        })
+    }
 }
