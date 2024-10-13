4b06b09f17670f234e65c91a7479b1c1f5213d02
Nurdin <mrsunnysummer@gmail.com>
2024-10-13 17:08:37 +0700

    feat: create event response and api service

--- app/src/main/java/com/nudriin/dicodingeventapp/data/retrofit/ApiService.kt
+++ app/src/main/java/com/nudriin/dicodingeventapp/data/retrofit/ApiService.kt
@@ -1,4 +1,11 @@
 package com.nudriin.dicodingeventapp.data.retrofit
 
+import com.nudriin.dicodingeventapp.data.response.EventResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiService {
+    @GET("events")
+    fun getAllEvents(@Query("active") active: String): Call<EventResponse>
 }
