c7e18b0e3ffbfe60cb3d1f0e1ffe8ab6ff2e8a87
Nurdin <mrsunnysummer@gmail.com>
2024-10-26 21:45:17 +0700

    feat: create settings fragment page

---
+++ app/src/main/java/com/nudriin/dicodingeventapp/ui/settings/SettingFragment.kt
@@ -0,0 +1,37 @@
+package com.nudriin.dicodingeventapp.ui.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
+import com.nudriin.dicodingeventapp.databinding.FragmentSettingBinding
+
+class SettingFragment : Fragment() {
+    private var _binding: FragmentSettingBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSettingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val switchTheme = binding.switchTheme
+
+        switchTheme.setOnCheckedChangeListener {_, isChecked: Boolean ->
+            if(isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                switchTheme.isChecked = true
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                switchTheme.isChecked = false
+            }
+
+        }
+    }
+}
